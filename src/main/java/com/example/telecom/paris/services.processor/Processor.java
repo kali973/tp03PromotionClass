643106e424edeb75113272ff828d7cfb798c1b8d
coutard <gilles.coutard@telecom-paris.fr>
2022-09-09 10:36:31 -0400

    Correction sur id à 1024

--- src/main/java/com/example/telecom/paris/services.processor/Processor.java
+++ src/main/java/com/example/telecom/paris/services.processor/Processor.java
@@ -4,7 +4,7 @@ import com.example.telecom.paris.models.Student;
 
 public class Processor {
     public static void main(String[] args) {
-        Student student = new Student(1024, "paul", "Edison");
+        Student student = new Student(1024, "Patrick", "Bellot");
         System.out.println(student);
     }
 }
